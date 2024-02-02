98ee22a8e5ada5d87d555851f2825bbf45a1c833
Jack <80669114+Argel-Tal@users.noreply.github.com>
2024-02-02 15:50:30 +1000

    added warlord to the fake game generator

--- fakeGameGenerator.R
+++ fakeGameGenerator.R
@@ -11,12 +11,13 @@ DirPlayerLog <- file.path(getwd(), "records/player matches")
 ngames <- length(list.files(DirGamelog))
 
 ## tables for randomised selection
-Factions <- read.table(file = file.path(DirSupportTbs, files[1]), sep = '\t', header = TRUE, quote = "")
-ForceOrgs <- read.table(file = file.path(DirSupportTbs, files[2]), sep = '\t', header = TRUE, quote = "")
-Maps     <- read.table(file = file.path(DirSupportTbs, files[3]), sep = '\t', header = TRUE, quote = "")
-Missions <- read.table(file = file.path(DirSupportTbs, files[4]), sep = '\t', header = TRUE, quote = "")
-Subfactions <- read.table(file = file.path(DirSupportTbs, files[5]), sep = '\t', header = TRUE, quote = "")
-WarlordTraits <- read.table(file = file.path(DirSupportTbs, files[6]), sep = '\t', header = TRUE, quote = "")
+Warlords  <- read.table(file = file.path(DirSupportTbs, files[1]), sep = '\t', header = TRUE, quote = "")
+Factions  <- read.table(file = file.path(DirSupportTbs, files[2]), sep = '\t', header = TRUE, quote = "")
+ForceOrgs <- read.table(file = file.path(DirSupportTbs, files[3]), sep = '\t', header = TRUE, quote = "")
+Maps      <- read.table(file = file.path(DirSupportTbs, files[4]), sep = '\t', header = TRUE, quote = "")
+Missions  <- read.table(file = file.path(DirSupportTbs, files[5]), sep = '\t', header = TRUE, quote = "")
+Subfactions <- read.table(file = file.path(DirSupportTbs, files[6]), sep = '\t', header = TRUE, quote = "")
+WarlordTraits <- read.table(file = file.path(DirSupportTbs, files[7]), sep = '\t', header = TRUE, quote = "")
 ptSizes <- seq(1000,5000, 50)
 
 gameHeaders <- c("gameSize", "mission", "deployment")
@@ -24,26 +25,18 @@ playerHeaders <- c("faction","forceOrg","ROW/Subfaction","alliedFaction"
                    ,"warlordTrait","VPs","warlord","listName"
                   ) # end headers
 
-## Loop params
-maxGenGames <- 15
-index <- 1
-## Loop 
-for(index in 1:maxGenGames){
+MakeGameMeta <- function(index){
   # game meta
   map_mission <- sample(1:6, 2, replace=T)
   gameSize    <- sample(ptSizes, 1)
   meta <- as.data.frame(t(c(gameSize, map_mission)))
   colnames(meta) <- gameHeaders
-  index = index + 1
-  write.csv(meta, paste(DirGamelog, "/", str_pad(index+ngames, 4, pad = "0"), ".csv", sep = ""))
-  # player 1
-  for (playerCount in 1:2)
+  return(meta)
+} # end MakeGameMeta
+ 
   
-  # player 2
-}
-
-MakePlayerMeta <- function(){
-  playerInitials <- paste(sample(LETTERS, 2, replace=TRUE), collapse="")
+  
+MakePlayerMeta <- function(index){
   faction <- Factions[sample(1:dim(Factions)[1], 1),2]
   parentFaction <- Factions[match(faction,Factions[,2]), 1]
   if (parentFaction == "Mechanicum"){
@@ -51,8 +44,14 @@ MakePlayerMeta <- function(){
   } else { 
     forceOrg <- ForceOrgs[sample(2:dim(ForceOrgs)[1], 1), 1]
   }
-  # pick subfactions they're allowed to choose from
-  ROW <- Subfactions[sample(which(Subfactions[,2] == parentFaction), 1), 1]
+  # not all factions have subs, so need to do error handling for the ones which dont
+  listOfSubFactions <- c(Subfactions[,2] == parentFaction)
+  if (any(listOfSubFactions)) {
+    # pick subfactions they're allowed to choose from
+    ROW <- Subfactions[sample(which(listOfSubFactions), 1), 1]
+  } else {
+    ROW <- ""
+  }
   # randomly determine if an allied detach is present, and if so which
   if (sample(1:100, 1)%%4 == 0) {
     alliedFaction <- Factions[sample(1:dim(Factions)[1], 1),2]
@@ -60,11 +59,57 @@ MakePlayerMeta <- function(){
     alliedFaction <- ""
   }
   trait   <- WarlordTraits[sample(1:dim(WarlordTraits)[1], 1),3]
-  warlord <- "jimmy space"
+  warlord <- Warlords[sample(which(c(Warlords[,2] == parentFaction)), 1), 1]
   VPs     <- sample(1:15, 1)
   listName    <- ""
   # dataframe stuff
   playerLog <- as.data.frame(t(c(faction, forceOrg, ROW, alliedFaction, trait, VPs, warlord, listName)))
   colnames(playerLog) <- playerHeaders
   return(playerLog)
+} # end MakePlayerMeta
+
+## Loop params
+maxGenGames <- 15
+index <- 1
+## Loop 
+for(index in 1:maxGenGames){
+  # game meta
+  write.csv(MakeGameMeta()
+            , paste(DirGamelog, "/", str_pad(
+                index+ngames, 4, pad = "0"
+                )
+              , ".csv", sep = ""
+              )
+            , row.names=FALSE
+            )
+  # player 1
+  write.csv(MakePlayerMeta()
+            , paste(
+              DirPlayerLog, "/"
+              , str_pad(
+                index+ngames, 4, pad = "0"
+              )
+              , "-"
+              , paste(sample(LETTERS, 2, replace=TRUE), collapse="") # 2 random initals
+              , ".csv"
+              , sep = ""
+            ) # end paste
+            , row.names=FALSE
+  ) # end write CSV
+  # player 2
+  write.csv(MakePlayerMeta()
+            , paste(
+              DirPlayerLog, "/"
+              , str_pad(
+                index+ngames, 4, pad = "0"
+              )
+              , "-"
+              , paste(sample(LETTERS, 2, replace=TRUE), collapse="") # 2 random initals
+              , ".csv"
+              , sep = ""
+            ) # end paste
+            , row.names=FALSE
+  ) # end write CSV
+  # update Index
+  index = index + 1
 }
